2bafc5ecdfecf7bf6bdf026dc8481c4aabe4fa81
AGitBoy <aidanwillie0317@protonmail.com>
2019-01-20 00:04:54 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Adpt
+This project prints out the adapter status on Linux
+
+### License
+This project is available under the Gplv3 or above
+
---
+++ adpt.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2019 Aidan Williams
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "adpt.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <getopt.h>
+
+static const char *versionstr = "adpt v1.0\n"
+                                "Copyright (C) 2019 Aidan Williams\n"
+                                "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>\n"
+                                "This is free software: you are free to change and redistribute it.\n"
+                                "There is NO WARRANTY, to the extent permitted by the law.\n\n"
+                                "Written by Aidan Williams\n";
+
+static const char *usagestr = "Usage: %s [OPTIONS]\n\n"
+                              "-v, --version\tPrints the version information\n"
+                              "-h, --help   \tPrints this help text\n";
+
+static struct option long_options[] = {
+    { "help",    no_argument, NULL, 'h' },
+    { "version", no_argument, NULL, 'v' },
+    { NULL,      0,           0,    0   }
+};
+
+void usage(char *progpth, int err) {
+    fprintf(err ? stderr : stdout, usagestr, progpth);
+    exit(err);
+}
+
+static void version() {
+    printf(versionstr);
+    exit(0);
+}
+
+int sysfsadptstat() {
+    FILE* fptr;
+    int plugged; 
+
+    fptr = fopen("/sys/class/power_supply/ADP0/online", "r");
+    
+    if(fptr) {
+        fscanf(fptr, "%d", &plugged);
+        fclose(fptr);
+    } else {
+        fprintf(stderr, "Error opening file: %s", "/sys/class/power_supply/ADP0/online");
+        exit(EIO);
+    }
+    
+    return plugged;
+}
+
+int adptstatus() {
+    return sysfsadptstat();
+}
+
+int main(int argc, char **argv) {
+    int plug;
+    int opt;
+
+    while((opt = getopt_long(argc, argv, ":hv", long_options, NULL)) != -1) {
+        switch(opt) {
+            case 'h':
+                usage(argv[0], EXIT_SUCCESS);
+            case 'v':
+                version();
+            default:
+                usage(argv[0], EINVAL);
+        }
+    }
+
+    plug = adptstatus();
+    
+    if(plug) {
+        puts("+");
+    } else {
+        puts("");
+    }
+
+    return 0;
+}
