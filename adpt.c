cdccfce76a07c6c8bc2e6985c5d27236a8a48c36
AGitBoy <aidanwillie0317@protonmail.com>
2019-04-01 22:30:13 -0400

    Updated style

--- adpt.c
+++ adpt.c
@@ -34,43 +34,43 @@
 #endif
 
 static const char *versionstr =
-	"adpt v1.0\n"
-	"Copyright (C) 2019 Aidan Williams\n"
-	"License GPLv3+: GNU GPL version 3 or later "
-	"<https://gnu.org/licenses/gpl.html>\n"
-	"This is free software: you are free to change and redistribute it.\n"
-	"There is NO WARRANTY, to the extent permitted by the law.\n\n"
-	"Written by Aidan Williams";
+    "adpt v1.0\n"
+    "Copyright (C) 2019 Aidan Williams\n"
+    "License GPLv3+: GNU GPL version 3 or later "
+    "<https://gnu.org/licenses/gpl.html>\n"
+    "This is free software: you are free to change and redistribute it.\n"
+    "There is NO WARRANTY, to the extent permitted by the law.\n\n"
+    "Written by Aidan Williams";
 
 static const char *usagestr =
-	"Usage: %s [OPTIONS]\n\n"
-	"-v, --version    \tPrints the version information\n"
-	"-h, --help       \tPrints this help text\n"
-	"-o, --on-string  \tSpecifies the string used when adapter is plugged in\n"
-	"-f, --off-string \tSpecifies the string used when adapter isn't plugged in\n";
+    "Usage: %s [OPTIONS]\n\n"
+    "-v, --version    \tPrints the version information\n"
+    "-h, --help       \tPrints this help text\n"
+    "-o, --on-string  \tSpecifies the string used when adapter is plugged in\n"
+    "-f, --off-string \tSpecifies the string used when adapter isn't plugged in\n";
 
 static struct option long_options[] = {
-	{ "help",       no_argument,       NULL, 'h' },
-	{ "version",    no_argument,       NULL, 'v' },
-	{ "on-string",  required_argument, NULL, 'o' },
-	{ "off-string", required_argument, NULL, 'f' },
-	{ NULL,         0,                 NULL, 0   }
+    { "help",       no_argument,       NULL, 'h' },
+    { "version",    no_argument,       NULL, 'v' },
+    { "on-string",  required_argument, NULL, 'o' },
+    { "off-string", required_argument, NULL, 'f' },
+    { NULL,         0,                 NULL, 0   }
 };
 
 
 // Prints usage information
 static void usage(char *progpth, int err)
 {
-	fprintf(err ? stderr : stdout, usagestr, progpth);
-	exit(err);
+    fprintf(err ? stderr : stdout, usagestr, progpth);
+    exit(err);
 }
 
 
 // Prints version information
 static void version()
 {
-	puts(versionstr);
-	exit(EXIT_SUCCESS);
+    puts(versionstr);
+    exit(EXIT_SUCCESS);
 }
 
 
@@ -78,53 +78,53 @@ static void version()
 // Internal for getting status of sysfs adapters
 int sysfsadptstat()
 {
-	FILE *fptr;
-	int plugged; 
-	
-	if ((fptr = fopen("/sys/class/power_supply/ADP0/online", "r"))) {
-		fscanf(fptr, "%d", &plugged);
-		fclose(fptr);
-	} else {
-		fprintf(
-			stderr,
-			"%s: %s",
-			strerror(errno),
-			"/sys/class/power_supply/ADP0/online"
-		);
+    FILE *fptr;
+    int plugged; 
+    
+    if ((fptr = fopen("/sys/class/power_supply/ADP0/online", "r"))) {
+        fscanf(fptr, "%d", &plugged);
+        fclose(fptr);
+    } else {
+        fprintf(
+            stderr,
+            "%s: %s",
+            strerror(errno),
+            "/sys/class/power_supply/ADP0/online"
+        );
 
-		exit(errno);
-	}
-	
-	return plugged;
+        exit(errno);
+    }
+    
+    return plugged;
 }
 #endif
 
 #if defined(__FreeBSD__)
 int bsdadptstat() 
 {
-	static int acpi;
-	int status;
+    static int acpi;
+    int status;
 
-	acpi = open("/dev/acpi", O_RDWR);
-	if (acpi < 0)
-		acpi = open("/dev/acpi", O_RDONLY);
-	
-	if (acpi < 0) {
-		fprintf(stderr, "%s: %s\n", strerror(errno), "/dev/acpi");
-		exit(errno);	
-	}
-	
-	if (ioctl(acpi, ACPIIO_ACAD_GET_STATUS, &status) < 0) {
-		fprintf(
-			stderr,
-			"ioctl error for adapter: %s",
-			explain_ioctl(acpi, ACPIIO_ACAD_GET_STATUS, &status)
-		);
+    acpi = open("/dev/acpi", O_RDWR);
+    if (acpi < 0)
+        acpi = open("/dev/acpi", O_RDONLY);
+    
+    if (acpi < 0) {
+        fprintf(stderr, "%s: %s\n", strerror(errno), "/dev/acpi");
+        exit(errno);    
+    }
+    
+    if (ioctl(acpi, ACPIIO_ACAD_GET_STATUS, &status) < 0) {
+        fprintf(
+            stderr,
+            "ioctl error for adapter: %s",
+            explain_ioctl(acpi, ACPIIO_ACAD_GET_STATUS, &status)
+        );
 
-		exit(EXIT_FAILURE);
-	}
+        exit(EXIT_FAILURE);
+    }
 
-	return status;
+    return status;
 }
 #endif
 
@@ -132,40 +132,40 @@ int bsdadptstat()
 int adptstatus()
 {
 #if defined(__linux)
-	return sysfsadptstat();
+    return sysfsadptstat();
 #elif defined(__FreeBSD__)
-	return bsdadptstat();
+    return bsdadptstat();
 #else
-	fputs("Unsupported platform");
-	exit(EXIT_FAILURE);
+    fputs("Unsupported platform");
+    exit(EXIT_FAILURE);
 #endif
 }
 
 
 int main(int argc, char **argv)
 {
-	int opt;
-	char *onstr = ONSTR;
-	char *offstr = OFFSTR;
+    int opt;
+    char *onstr = ONSTR;
+    char *offstr = OFFSTR;
 
-	while ((opt = getopt_long(argc, argv, ":hvf:o:", long_options, NULL)) != -1) {
-		switch (opt) {
-		case 'h':
-			usage(argv[0], 0);
-		case 'v':
-			version();
-		case 'o':
-			onstr = optarg;
-			break;
-		case 'f':
-			offstr = optarg;
-			break;
-		default:
-			usage(argv[0], EINVAL);
-		}
-	}
-	
-	puts(adptstatus() ? onstr : offstr);
+    while ((opt = getopt_long(argc, argv, ":hvf:o:", long_options, NULL)) != -1) {
+        switch (opt) {
+        case 'h':
+            usage(argv[0], 0);
+        case 'v':
+            version();
+        case 'o':
+            onstr = optarg;
+            break;
+        case 'f':
+            offstr = optarg;
+            break;
+        default:
+            usage(argv[0], EINVAL);
+        }
+    }
+    
+    puts(adptstatus() ? onstr : offstr);
 
-	return 0;
+    return 0;
 }
